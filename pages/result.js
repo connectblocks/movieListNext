8fa84aa5dc17cac10dc276d4a3744ad9b5e8b1fc
michael <michael.shin.hrr@gmail.com>
2018-11-06 11:30:06 -0500

    wip: d3 graph

--- pages/result.js
+++ pages/result.js
@@ -10,43 +10,75 @@ class Result extends Component {
   }
 
   drawChart() {
-    const data = [12, 8];
-    const w = '200px'
-    const h = 100
-    const value = 'y'
+  if(!this.props.movie.budget || !this.props.movie.revenue) {
+    return; 
+  }
+    const data = [{name: 'budget', value: this.props.movie.budget},{name: 'revenue', value: this.props.movie.revenue} ];
+    const w = 500;
+    const h = 300;
     
+    const yScale = d3.scaleLinear().domain([0, d3.max(data, (d) => d.value)]).range([h, 0]);
+    const xScale = d3.scaleLinear().domain([0, w]).range([0, w]);
+    const yAxis = d3.axisLeft(yScale);
+    const xAxis = d3.axisBottom(xScale);
+
     const svg = d3.select("#chart")
     .append("svg")
     .attr("width", w)
     .attr("height", h)
-                  
+
     svg.selectAll("rect")
       .data(data)
       .enter()
       .append("rect")
-      .attr("x", (d, i) => i * 100)
-      .attr("y", (d, i) => h - 10 * d)
-      .attr("width", 20)
-      .attr("height", (d, i) => d * 10)
-      .attr("fill", value === 'x' ? "blue": "red")
+      .attr("x", (d, i) => i * w / 3)
+      .attr("y", (d, i) => yScale(d.value))
+      .attr("width", w / 4)
+      .attr("height", (d, i) => h - yScale(d.value))
+      .attr("fill", (d, i) => d.name === 'budget' ? '#482880' : '#ffc400')
    
-      svg.selectAll("text")
-       .data(data)
-       .enter()
-       .append("text")
-       .text((d) => d)
-       .attr("x", (d, i) => i * 100)
-       .attr("y", (d, i) => h - (d * 3 )) 
+    svg.selectAll("text")
+      .data(data)
+      .enter()
+      .append("text")
+      .text((d) => d.value)
+      .attr("x", (d, i) => i * w / 3)
+      .attr("y", (d, i) => 200) 
+    
+    svg.append("g")
+      .attr("transform", `translate(${w - 50}, 0)`)
+      .call(yAxis);
+
+    svg.append("g")        
+      .attr("transform", `translate(0, ${h})`)
+      .call(xAxis);
   }
 
-  
   render() {
     return (
       <div>
-        <div>{this.props.movie.title}</div>
-        <div id='chart'></div>
+        <h2>{this.props.movie.title}</h2>
+        <div><b>Release Date: </b>{this.props.movie.release_date}</div>
+        <div><b>Popularity: </b>{this.props.movie.popularity}</div>
+        <div>
+          <b>Overview: </b>
+          <div>{this.props.movie.overview}</div>
+        </div>
+        <div>
+          <b>Production Companies </b>
+          <div>{this.props.movie.production_companies && this.props.movie.production_companies.map(el => <li key={el.id}>{el.name}</li>)}</div>
+        </div> 
+        { 
+          this.props.movie.budget && this.props.movie.revenue 
+          ? (
+              <div>
+                <b>Budget and Revenue </b>
+                <div id='chart'></div>
+              </div>
+            )
+          : null
+        }
       </div>
-      
     )
   }
 }
@@ -59,39 +91,3 @@ Result.getInitialProps = async function (context) {
 }
 
 export default Result
-
-// const Result = (props) => {
-//   const budget = props.movie.budget;
-//   const revenue = props.movie.revenue;
-//   const square = d3.select(refs.container)
-//   return (
-//     <div>
-//       <h1>{props.movie.title}</h1>
-//       <div>{props.movie.tagline}</div>
-//       <div>{props.movie.overview}</div>
-//       <div>{budget}</div>
-//       <div>{revenue}</div>
-//       <div>{square}</div>
-//       <svg with={900} height={900} ref='container'></svg>
-//       <style jsx>{`
-//         .bar {
-//           width: 25px;
-//           height: 100px;
-//           display: inline-block;
-//           background-color: blue;
-//         }
-//         `}
-//         </style>
-
-//     </div>
-//     )
-// }
-
-// Result.getInitialProps = async function (context) {
-//   const { id } = context.query;
-//   const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${CONFIG.movie_db_api_key}`)
-//   const movie = await res.json();
-//   return { movie }
-// }
-
-// export default Result
